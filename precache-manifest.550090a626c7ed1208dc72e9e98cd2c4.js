8c1338d184c298fc77b391b0354ff3e3702a1736
Hafiz-Azam <SP20-BSE-110@cuilahore.edu.pk>
2022-08-29 01:41:37 +0500

    Updates

---
+++ precache-manifest.550090a626c7ed1208dc72e9e98cd2c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec5f7377f307c3ee18fd",
+    "url": "/my-app/static/css/main.5f0c1465.chunk.css"
+  },
+  {
+    "revision": "ec5f7377f307c3ee18fd",
+    "url": "/my-app/static/js/main.ec5f7377.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/my-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/my-app/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "ad6099f2e84041bd8e59",
+    "url": "/my-app/static/js/runtime~main.ad6099f2.js"
+  },
+  {
+    "revision": "695412fde1e2713ffe8976fe4c950116",
+    "url": "/my-app/index.html"
+  }
+];
